668dea3be48bf07b9f80965a7cb860c26253cefc
saiashish9 <saiashish3760@gmail.com>
2021-01-02 00:36:28 +0530

    initial commit

---
+++ src/test/java/com/springswagger/springswagger/SpringswaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springswagger.springswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringswaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
